6a36d81bf2eb165cc8bfadb2fdbce31941fccfdd
Niklas Wang <niklas.wang@outlook.com>
2021-07-11 19:03:05 +0800

    [Jul-11] Implement server request handler

--- server/RequestHandler.h
+++ server/RequestHandler.h
@@ -1,22 +1,26 @@
 #ifndef _REQUEST_HANDLER_H_
 #define _REQUEST_HANDLER_H_
 
-#include "common.h"
-#include "ServerIntf.h"
-#include "HandlerOpsIntf.h"
+#include "Common.h"
 #include "RequestHandlerIntf.h"
+#include "ServerIntf.h"
+#include "CallbackThread.h"
 #include "ThreadPoolEx.h"
 #include "SocketServerStateMachine.h"
 
 namespace voyager {
 
 class RequestHandler :
-    public RequestHandlerIntf {
+    public RequestHandlerIntf,
+    public Identifier,
+    public noncopyable {
 public:
-    virtual int32_t enqueue(int32_t id) override;
+
+    virtual int32_t onClientReady(int32_t clientfd, const std::string &privateMsg) override;
+    virtual int32_t enqueue(int32_t fd) override;
+    virtual int32_t enqueue(void *dat) = 0;
+    virtual int32_t enqueue(void *dat, int32_t format) = 0;
     virtual int32_t abort() override;
-    virtual int32_t setSocketFd(int32_t fd) override;
-    virtual int32_t onClientReady() override;
 
     virtual RequestType getType() override;
     virtual const char *getName() override;
@@ -24,52 +28,29 @@ public:
 public:
     virtual int32_t construct() override;
     virtual int32_t destruct() override;
-    RequestHandler(HandlerOpsIntf *ops,
-        RequestType type, const char *name, uint32_t memNum);
+    RequestHandler(RequestType type, const char *name, CallbackIntf *cb);
     virtual ~RequestHandler();
 
 protected:
-    virtual int32_t getHeaderSize() = 0;
-    virtual int32_t getDataSize() = 0;
-    virtual int32_t sendFreshData(
-        RequestType type, int32_t id, void *head, void *dat);
-
-private:
-    virtual int32_t startServerLoop();
+    virtual int32_t onClientSent(int32_t fd, const std::string &privateMsg) = 0;
+    virtual int32_t startServerLoop(int32_t clientfd, const std::string &privateMsg);
     virtual int32_t exitServerLoop();
-    int32_t getExpectedBufferSize();
-    int32_t allocMemAndShare();
-    int32_t allocMem();
-    int32_t releaseMem();
-    int32_t shareSingleMem(int32_t fd);
-    int32_t shareMem();
-    int32_t onMemRefreshed(int32_t fd);
-    int32_t recordMemoryPair(int32_t serverfd, int32_t clientfd);
-    int32_t convertToClientFd(char *msg, const char *prefix, int32_t *clientfd);
-
-private:
-    struct MemoryInfo {
-        int32_t id;
-        void   *buf;
-        int32_t size;
-        int32_t serverfd;
-        int32_t clientfd;
-    };
-
-private:
-    bool          mConstructed;
-    const char   *mName;
-    RequestType   mType;
-    bool          mMemShared;
-    uint32_t      mMemNum;
-    MemoryInfo   *mMem;
-    ThreadPoolEx *mThreads;
+    bool checkFdExists(int32_t fd);
+    bool checkFdExists(void *ptr);
+    void removeFdRecord(int32_t fd);
+    void removeFdRecord(void *ptr);
+    void addFdRecord(int32_t fd);
+    void addFdRecord(int32_t fd, void *ptr);
 
 protected:
-    ModuleType      mModule;
-    Header          mHeader;
-    HandlerOpsIntf *mOps;
-    SocketServerStateMachine mSSSM;
+    bool          mConstructed;
+    std::string   mName;
+    RequestType   mType;
+    CallbackIntf *mCb;
+    ThreadPoolEx *mThreads;
+    SocketServerStateMachine  mSSSM;
+    BufferMgr     mBufMgr;
+    std::map<int32_t, void *> mFds;
 };
 
 };
--- server/RequestHandlerIntf.h
+++ server/RequestHandlerIntf.h
@@ -8,15 +8,14 @@ namespace voyager {
 class RequestHandlerIntf {
 public:
 
-    virtual int32_t enqueue(int32_t id) = 0;
+    virtual int32_t onClientReady(int32_t clientfd, const std::string &privateMsg) = 0;
+    virtual int32_t enqueue(void *dat) = 0;
+    virtual int32_t enqueue(int32_t fd) = 0;
+    virtual int32_t enqueue(void *dat, int32_t format) = 0;
     virtual int32_t abort() = 0;
-    virtual int32_t onClientReady(int32_t clientfd, std::string privateMsg) = 0;
 
     virtual RequestType getType() = 0;
     virtual const char *getName() = 0;
-
-    virtual int32_t construct() = 0;
-    virtual int32_t destruct() = 0;
 
 public:
     virtual ~RequestHandlerIntf() {}
